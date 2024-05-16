0f885027f20b8c5f7e472f57d6398117e4fdfc47
bashik-a00 <bashiksultan94@gmail.com>
2024-05-16 13:52:34 +0530

    registration setup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,18 @@
 const App=()=>{
   return(
-    <div>hloo</div>
+    <div className="container">
+      <h1 id="">Registeration</h1>
+      <form action="" id="reg-form">
+        <div className="form-grp">
+        <label htmlFor="username">Username:</label>
+        <input type="text" id="username" name="username" required/>
+        </div>
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" name="email"/>
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" name="password" required/>
+      </form>
+    </div>
   );
 }
 
